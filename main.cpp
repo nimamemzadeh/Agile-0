73db4d8fd3580c15849dc3eddda0b6e66689f346
nimamemzadeh <64117019+nimamemzadeh@users.noreply.github.com>
2020-08-25 19:09:36 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "header.h"
+using namespace std;
+
+int main()
+{
+
+
+  cout << "Welcome to Agile0" << endl;
+  cout << "Development team:" << endl;
+  feauture1();
+  feauture2();
+  feauture3();
+  feauture4();
+  feauture5();
+  
+  return 0;
+
+}
