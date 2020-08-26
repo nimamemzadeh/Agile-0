ec2d5a7742cd94a9cf1e8598abbeca4f273ecef1
I-Am-I-Am <70232542+I-Am-I-Am@users.noreply.github.com>
2020-08-25 22:30:24 -0700

    Update feature5.cpp
    Changed "My name is Andres Perez" to "Andres Perez".

--- feature5.cpp
+++ feature5.cpp
@@ -3,12 +3,12 @@
 
 /*
 This is a feature and not a bug.
-If message "My Name is Andres Perez" is
+If message "Andres Perez" is
 being displayed then the feature5 is working
 */
 void feature5()		
 {					
 					
-  cout << "My Name is Andres Perez"<<endl;
+  cout << "Andres Perez" << endl;
   
 }
